0e175995f759b633b8ea1a849d28d533b908dc0e
Maiero <matteo.maiero@eurotech.com>
2018-04-09 17:41:03 +0200

    Updated license to EPL v2
    Signed-off-by: Maiero <matteo.maiero@eurotech.com>

--- node-rest-proxy/rest-proxy.js
+++ node-rest-proxy/rest-proxy.js
@@ -1,11 +1,13 @@
 /*******************************************************************************
- *Copyright (C) 2017 Eurotech.
- *All rights reserved. This program and the accompanying materials
- *are made available under the terms of the Eclipse Public License v1.0
- *which accompanies this distribution, and is available at
- *http://www.eclipse.org/legal/epl-v10.html
+ * Copyright (C) 2017, 2018 Eurotech.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
  *
- *Contributors:
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
  *    Eurotech - initial API and implementation
  ******************************************************************************/
 var http = require('http'),
